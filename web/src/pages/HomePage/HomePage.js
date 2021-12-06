804a764ebebd08b16a9ecd8027544cc01081418b
Amanda Giannelli <giannelli.tech@gmail.com>
2021-12-05 19:25:22 -0500

    Add BlogPostsCell and update HomePage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
-  return (
-    <>
-      <p>Home</p>
-    </>
-  )
+  return <BlogPostsCell />
 }
 
 export default HomePage
